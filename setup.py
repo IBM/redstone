7427912a30f9edd2f8a63b69fd5fbc82edfe99db
Mathew Odden <locke105@gmail.com>
2019-03-27 01:26:13 -0500

    Rev version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="redstone",
-    version="0.1.1",
+    version="0.1.2",
     author="Mathew Odden",
     author_email="mathewrodden@gmail.com",
     url="https://github.com/locke105/redstone",
