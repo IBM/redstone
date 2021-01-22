7b9996366d02f5506ea02286b8975be1a7418656
thu <thu@ibm.com>
2021-01-22 10:37:42 -0600

    Add key alias functions and test

--- redstone/client.py
+++ redstone/client.py
@@ -605,6 +605,18 @@ class KeyProtect(BaseClient):
 
         return self._action(key_id, "rotate", data)
 
+    def create_key_alias(self, key_id: str, alias: str):
+        """Creates an alias for the specified key"""
+        resp = self.session.post("%s/api/v2/keys/%s/aliases/%s" % (self.endpoint_url, key_id, alias))
+        self._validate_resp(resp)
+        return resp.json()
+
+    def delete_key_alias(self, key_id: str, alias: str):
+        """Deletes an alias from the associated key"""
+        resp = self.session.delete("%s/api/v2/keys/%s/aliases/%s" % (self.endpoint_url, key_id, alias))
+        self._validate_resp(resp)
+        return resp.status_code
+
 
 class CISAuth(requests.auth.AuthBase):
     def __init__(self, credentials):
---
+++ test/integration/test_keyprotect.py
@@ -0,0 +1,55 @@
+import logging
+import unittest
+import os
+import warnings
+
+import redstone
+from redstone import auth as bxauth
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+class KeyProtectTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # filter out the resource warning message
+        warnings.simplefilter("ignore", ResourceWarning)
+
+        apikey = os.environ.get('IBMCLOUD_API_KEY')
+        tm = bxauth.TokenManager(api_key=apikey)
+        self.rc = redstone.service("ResourceController")
+        self.instance_id, self.crn = self.rc.create_instance(name="test-instance", plan_id=self.rc.KEYPROTECT_PLAN_ID,
+                                                             region="us-south")
+        self.kp = redstone.service("KeyProtect",
+                                   credentials=tm,
+                                   region="us-south",
+                                   service_instance_id=self.instance_id,
+                                   )
+
+    def tearDown(self):
+        for key in self.kp.keys():
+            self.kp.delete(key.get('id'))
+
+        self.rc.delete_instance(self.instance_id)
+        self.kp.session.close()
+
+    def test_key_alias(self):
+        # test creating key alias
+        self.key = self.kp.create(name="test-key", root=True)
+        resp = self.kp.create_key_alias(self.key['id'], "testKeyAlias")
+        self.assertEqual(resp['resources'][0]['keyId'], self.key['id'])
+        self.assertEqual(resp['resources'][0]['alias'], "testKeyAlias")
+        # test getting key by using the key alias
+        resp = self.kp.get("testKeyAlias")
+        self.assertEqual(resp['id'], self.key['id'])
+        self.assertEqual(resp['aliases'][0], "testKeyAlias")
+        # test deleting key alias
+        resp = self.kp.delete_key_alias(self.key['id'], "testKeyAlias")
+        self.assertEqual(resp, 204)
+        # clean up
+        self.kp.delete(self.key.get('id'))
+
+
+if __name__ == "__main__":
+    unittest.main()
