0de8e43e6cbde2a18dda28b3515d016d219a3e70
thu <thu@ibm.com>
2021-02-15 14:58:48 -0600

    Add API doc links

--- redstone/client.py
+++ redstone/client.py
@@ -612,6 +612,9 @@ class KeyProtect(BaseClient):
         If `key_id` is None (the default) all registrations for the instance are
         returned, otherwise only the registrations associated with a specified root
         key are returned.
+
+        API Docs: https://cloud.ibm.com/apidocs/key-protect#getregistrations
+                  https://cloud.ibm.com/apidocs/key-protect#getregistrationsallkeys
         """
         params = {}
         if crn is not None:
