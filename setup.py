6d2e094d81604eecb89693542efd0353aef00a1c
Matt Odden <mathewrodden@gmail.com>
2021-03-10 23:17:47 +0000

    Bump version for 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="redstone",
-    version="0.3.0",
+    version="0.4.1",
     author="Mathew Odden",
     author_email="mathewrodden@gmail.com",
     url="https://github.com/mrodden/redstone",
